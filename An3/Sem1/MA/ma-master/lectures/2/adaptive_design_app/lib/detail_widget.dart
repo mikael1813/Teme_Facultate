a4840e6e1aca14d5d0b94f17b2037af27ee496d4
mikael1813 <mihaibogdan1812@gmail.com>
2022-05-21 15:32:29 +0300

    commit_all

---
+++ An3/Sem1/MA/ma-master/lectures/2/adaptive_design_app/lib/detail_widget.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class DetailWidget extends StatefulWidget {
+
+  final int data;
+
+  const DetailWidget(this.data, {Key? key}) : super(key: key);
+
+  @override
+  _DetailWidgetState createState() => _DetailWidgetState();
+}
+
+class _DetailWidgetState extends State<DetailWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.blue,
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(widget.data.toString(), style: TextStyle(fontSize: 36.0, color: Colors.white),),
+          ],
+        ),
+      ),
+    );
+  }
+}
